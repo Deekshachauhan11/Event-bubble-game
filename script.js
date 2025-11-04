43f3a339bdc02ea06accf24da2c9bd06f5b8e623
Deeksha Chauhan <cdeeksha0011@gmail.com>
2025-11-04 12:41:18 +0530

    complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let timer = 60;
 let score = 0;
-
+let hitrn = 0;
 
 function makeBubble() {
   let clutter = "";
@@ -14,34 +14,43 @@ function makeBubble() {
 }
 
 function getHit() {
-    let rn = Math.floor(Math.random()*10)
-    document.querySelector("#hitval").textContent = rn;
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = hitrn;
 }
 
-
 function runTimer() {
-  let timerint = setTimeout(function () {
+  let timerint = setInterval(function () {
     if (timer > 0) {
       timer--;
       document.querySelector("#timerval").textContent = timer;
     } else {
       clearTimeout(timerint);
+      document.querySelector(".bottom-panel").innerHTML = `<h1>Game over</h1>`;
     }
   }, 1000);
 }
 
-
-function increaseScore(){
-    score += 10;
-    document.querySelector("#scoreval").textContent = score;
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
 }
 
 
-document.querySelector(".bottom-panel").addEventListener("click", function(dets){
-     console.log(Number(dets.target.textContent))
-});
+function game(){
+  document
+  .querySelector(".bottom-panel")
+  .addEventListener("click", function (dets) {
+    let clicked = Number(dets.target.textContent);
+    if (clicked === hitrn) {
+      increaseScore();
+      getHit();
+      makeBubble();
+    }
+  });
+}
+
 
 getHit();
 makeBubble();
 runTimer();
-
+game();
